be4d3b8db227b176e1e9a152e639d58a8c64a0d3
REMILI Brahim <mohamedremili5000@gmail.com>
2026-08-10 23:03:12 +0100

    feat: update About page content and layout with stylized text highlights and navigation arrows

--- App/src/components/About.jsx
+++ App/src/components/About.jsx
@@ -1,24 +1,34 @@
 import { Link } from "react-router-dom";
+import Arrows from "./Arrows";
+
 
 const About = () => {
     return (
-        <div className="bg-[#1E1E1E] min-h-screen flex flex-col items-center justify-center p-4">
-            <h1 className="text-4xl md:text-5xl font-bold text-white mb-6">
-                About Enncom Lite
-            </h1>
-            <p className="text-gray-300 text-lg text-center max-w-2xl leading-relaxed">
-                Enncom lite is a real-time chat application 
-                It allows users to communicate with each other in real-time.
+        <>
 
-            </p>
-           <h1>Our Vision</h1>
-           <p className="text-gray-300 text-lg text-center max-w-2xl leading-relaxed">
-            Our goal is to let pepole be confortable safe and free to speak chat and share ideas in a privacy friendly environment.
-            
+            <div className="bg-[#1E1E1E] min-h-screen flex flex-col items-center justify-center p-4">
+                <h1 className="text-4xl md:text-5xl font-bold text-white mb-6">
+                    About Enncom Lite
+                </h1>
+                <p className="text-gray-300 text-lg text-center max-w-2xl leading-relaxed">
+                    Enncom lite is a <span className="text-red-500 font-bold">free real-time chat</span> application.
+                    It allows users to communicate with each other in real-time.
+
+                </p>
+
+                <h1 className="text-4xl md:text-5xl font-bold text-white mb-6">Our Vision</h1>
+                <p className="text-gray-300 text-lg text-center max-w-2xl leading-relaxed">
+                    We belive in the <span className="text-red-500 font-bold">freedom of speech</span>, and the right for privacy
+                </p>
+                <p className="text-gray-300 text-lg text-center max-w-2xl leading-relaxed">
+                    Our goal is to let people be comfortable,  <span className="text-red-500 font-bold">safe</span> and free to speak chat and share ideas in a private friendly environment.
 
 
-           </p>
-        </div>
+
+                </p>
+            </div>
+
+        </>
     );
 };
 
