5dfe29ab8d03dd31b7414e143d546e7cc9f8c8d6
REMILI Brahim <mohamedremili5000@gmail.com>
2026-08-13 20:19:15 +0100

    feat: add Rooms component with private room creation button

---
+++ App/src/components/Rooms.jsx
@@ -0,0 +1,17 @@
+const Rooms = () => {
+    return (
+        <div>
+
+            <div className="flex gap-10">
+                <h1>Rooms</h1>
+                <button>Create a Private Room</button>
+            </div>
+            <div>
+
+            </div>
+
+        </div>
+    )
+}
+
+export default Rooms
